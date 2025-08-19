33f82913a36c559f84136f924dab514e25caee37
Andrew <andrewttm@live.com>
2025-08-19 23:06:17 +0800

    feat: add Redux integration, update TypeScript config, and remove Tailwind CSS plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { resolve, dirname } from "path";
-import tailwindcss from "@tailwindcss/vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: [
       { find: "@", replacement: resolve(__dirname, "src") },
